529d609366e79325469fa1d8f2883f0121f9afd8
sagar15 <sagar@finday.com>
2021-01-09 11:47:58 +0530

    added timer function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {interval, Subscription, timer} from 'rxjs';
+import {take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'test-angular-interval';
+export class AppComponent implements OnDestroy{
+  timer = timer(1000, 500);
+  subscription: Subscription;
+  output = [];
+
+  ngOnDestroy(): void {
+    this.stopInterval();
+  }
+
+  startInterval(): void {
+    if (this.subscription) {
+      this.stopInterval();
+    }
+    this.subscription = this.timer.subscribe((num) => {
+      this.output.push(num);
+      if (num === 15) {
+        this.subscription.unsubscribe();
+      }
+    });
+  }
+
+  stopInterval() {
+    this.subscription.unsubscribe();
+  }
 }
